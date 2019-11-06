388f50cd756a62f342c0951f1d241c97d5d11faa
unknown <49761803+Guilhermeallan@users.noreply.github.com>
2019-11-06 19:15:03 -0300

    Adicionar arquivos da primeira aula de Java Script

---
+++ aula06/script.js
@@ -0,0 +1,18 @@
+let login= "famoso171";
+let senha= 187495541;
+
+let loginUsuario= prompt("Digite o seu usuário");
+let senhaDoLogin= prompt("Digite a sua senha");
+
+
+if(login == loginUsuario && senha == senhaDoLogin){
+    alert ("Bem-vindo");
+}
+else {
+    alert ("Usuário ou senha incorretos");
+
+}
+
+while {
+
+}
