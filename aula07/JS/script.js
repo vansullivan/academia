37ddcc7354fd42e38c53258bbb3196d6810a9309
unknown <49761803+Guilhermeallan@users.noreply.github.com>
2019-11-06 22:11:09 -0300

    Adicionar for para criar escada no Java Script

---
+++ aula07/JS/script.js
@@ -0,0 +1,52 @@
+/*
+let animes = ['Death Note', 'Boku No Hero', 'Dragon Ball', 'Cavaleiros do Zodiíaco', 
+'Naruto', 'Super Onze', 'Nazo No Kanojo']
+
+let i = 3;
+
+while (i < animes.lenght){
+    console.log (animes[i]);
+    i++;
+}
+*/
+
+/*
+// while
+
+let animes = ['Death Note', 'Boku No Hero', 'Dragon Ball', 'Cavaleiros do Zodiíaco', 
+'Naruto', 'Super Onze', 'Nazo No Kanojo'] // lista
+
+for (let e= 0; e <animes.length; e++){
+    console.log(animes[e]);
+}
+*/
+
+/*
+let tintas = ['Aquarela', 'Acrílica', 'Nankim', 'Guachê TGA']
+
+for (let artista= 0; artista <animes.lenght; artista++){
+    console.log(animes[artista];)
+}
+*/
+
+/*
+function comprarPao
+(dinheiro, pao) {
+    receber o dinheiro e comprar o pão
+    return pão;
+}
+console.log(comprarPao())
+    comprarPao();
+*/
+
+let material = "#";
+for (i = 0; i < 5; i ++){
+    console.log(material + i)
+
+}
+
+
+
+
+
+
